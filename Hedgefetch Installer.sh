e58420f99569017b18067d9a6c84c20c5a8f5136
ASentientHedgehog <72932987+moosethegoose2213@users.noreply.github.com>
2021-04-28 21:08:39 -0500

    Support for OpenCore Legacy Patcher, and more
    Support for OpenCore Legacy Patcher, OpenCore, detection of monitors among other improvements

--- Hedgefetch Installer.sh
+++ Hedgefetch Installer.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e
 DIR=$(dirname "$0")
-echo $DIR >> /tmp/dir
 if [ -d /usr/local/bin ]; then
 	echo "Installing to /usr/local/bin"
-	cp "$DIR/hedgefetch" /usr/local/bin/hedgefetch
-#osascript -e 'do shell script "cp  " & (quoted form of (do shell script "cat /tmp/dir")) & "/hedgefetch /usr/local/bin/hedgefetch" with administrator privileges'
+	cp "$DIR/hedgefetch" /usr/local/bin/
 	INS=$(echo /usr/local/bin)
 else
-	echo "Installing to /bin"
-cp "$DIR/hedgefetch" /bin/hedgefetch
-#osascript -e 'do shell script "cp $DIR/hedgefetch /bin/hedgefetch" with administrator privileges'
-	INS=$(echo /bin)
-
+	echo "Creating /usr/local/bin"
+	osascript -e 'do shell script "mkdir -p /usr/local/bin" with administrator privileges'
+	echo "Installing to /usr/local/bin"
+	cp "$DIR/hedgefetch" /usr/local/bin/
+	INS=$(echo /usr/local/bin)
 fi
-
-if [ -e $INS/hedgefetch ]; then
-	echo 'Install successful! Try it out by running "hedgefetch" in terminal.'
-else
-	echo "Install failed successfully! Please try again, and if that doesn't work report this to GitHub."
-	echo "Heres a nice rickroll to cheer you up!"
-	open https://www.youtube.com/watch?v=dQw4w9WgXcQ
-fi
+echo "Finished!"
